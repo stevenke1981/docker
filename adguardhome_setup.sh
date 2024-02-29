4982e387d7ecd2a76e281feea189bce32918a677
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-02-29 23:00:41 +0800

    Update adguardhome_setup.sh

--- adguardhome_setup.sh
+++ adguardhome_setup.sh
@@ -1,42 +1,71 @@
 #!/bin/bash
-#version 1.1.1
-
-#set -euo pipefail # Enable error handling for robustness
-
-# Ensure root privileges
-if [[ $EUID -ne 0 ]]; then
- echo "此腳本需要 root 權限。請使用 sudo 執行。"
- exit 1
-fi
+#ver1.1.2
 
 # Define constants
 WORK_DIR="${HOME}/adguardhome/work"
 CONFIG_DIR="${HOME}/adguardhome/config"
 
-# Define function to remove AdGuard Home
-function remove_adguardhome() {
- echo "移除 AdGuard Home..."
+# Define function to install AdGuard Home
+function install_adguardhome() {
+  # Configure systemd-resolved
+  configure_systemd_resolved
 
- # Stop and remove container
- docker stop adguardhome
- docker rm adguardhome
+  # Ensure directories exist
+  if [ ! -d "$WORK_DIR" ]; then
+    echo "建立工作目錄：$WORK_DIR"
+    mkdir -p "$WORK_DIR"
+  fi
 
- # Remove configuration files
- rm -f /etc/systemd/resolved.conf.d/adguardhome.conf
+  if [ ! -d "$CONFIG_DIR" ]; then
+    echo "建立設定檔目錄：$CONFIG_DIR"
+    mkdir -p "$CONFIG_DIR"
+  fi
 
- # Remove directories
- rm -rf "$WORK_DIR" "$CONFIG_DIR"
+  # Run AdGuard Home in a Docker container
+  docker run \
+    --name adguardhome \
+    --restart unless-stopped \
+    -v "$WORK_DIR":/opt/adguardhome/work \
+    -v "$CONFIG_DIR":/opt/adguardhome/conf \
+    -p 53:53/tcp -p 53:53/udp \
+    -p 3000:3000/tcp \
+    -d adguard/adguardhome
 
- echo "AdGuard Home 已移除。"
+  echo "AdGuard Home 伺服器可於 http://$(hostname -I | awk '{print $1}'):3000 存取"
 }
 
-# Check if systemd-resolved is running
-if ! systemctl is-active systemd-resolved; then
- echo "systemd-resolved 沒有在執行。腳本執行已停止。"
- exit 0
-fi
+# Define function to remove AdGuard Home
+function remove_adguardhome() {
+  echo "移除 AdGuard Home..."
 
-# Configure systemd-resolved for AdGuard Home
+  # Stop and remove container
+  docker stop adguardhome
+  docker rm adguardhome
+
+  # Prompt user to keep or remove directories
+  while true; do
+    read -p "是否保留工作目錄 (${WORK_DIR}) 和設定檔目錄 (${CONFIG_DIR})？ [y/n] " keep_directories
+
+    case "$keep_directories" in
+      y|Y)
+        echo "保留資料夾。"
+        break
+        ;;
+      n|N)
+        echo "移除資料夾。"
+        rm -rf "$WORK_DIR" "$CONFIG_DIR"
+        break
+        ;;
+      *)
+        echo "無效的輸入。"
+        ;;
+    esac
+  done
+
+  echo "AdGuard Home 已移除。"
+}
+
+# Define function to configure systemd-resolved
 function configure_systemd_resolved() {
   if [ ! -f /etc/systemd/resolved.conf.d/adguardhome.conf ]; then
     echo "建立設定檔 /etc/systemd/resolved.conf.d/adguardhome.conf..."
@@ -50,50 +79,27 @@ DNSStubListener=no' | sudo tee /etc/systemd/resolved.conf.d/adguardhome.conf
 
 # Display menu and handle user input
 while true; do
- echo "** AdGuard Home 安裝和移除工具 **"
- echo "1. 安裝 AdGuard Home"
- echo "2. 移除 AdGuard Home (一鍵)"
- echo "3. 離開"
- read -p "輸入您的選擇： " choice
+  echo "** AdGuard Home 安裝和移除工具 **"
+  echo "1. 安裝 AdGuard Home"
+  echo "2. 移除 AdGuard Home"
+  echo "3. 離開"
+  read -p "輸入您的選擇： " choice
 
- case "$choice" in
-  1)
-   # Ensure directories exist
-   if [ ! -d "$WORK_DIR" ]; then
-    echo "建立工作目錄：$WORK_DIR"
-    mkdir -p "$WORK_DIR"
-   fi
-
-   if [ ! -d "$CONFIG_DIR" ]; then
-    echo "建立設定檔目錄：$CONFIG_DIR"
-    mkdir -p "$CONFIG_DIR"
-   fi
-
-   # Run AdGuard Home in a Docker container
-   docker run \
-    --name adguardhome \
-    --restart unless-stopped \
-    -v "$WORK_DIR":/opt/adguardhome/work \
-    -v "$CONFIG_DIR":/opt/adguardhome/conf \
-    -p 53:53/tcp -p 53:53/udp \
-    -p 3000:3000/tcp \
-    -d adguard/adguardhome
-
-   echo "AdGuard Home 伺服器可於 http://$(hostname -I | awk '{print $1}'):3000 存取"
-   break
-   ;;
-  2)
-   remove_adguardhome
-   break
-   ;;
-  3)
-   echo "離開..."
-   exit 0
-   ;;
-  *)
-   echo "無效的選擇。"
-   ;;
- esac
+  case "$choice" in
+    1)
+      install_adguardhome
+      break
+      ;;
+    2)
+      remove_adguardhome
+      break
+      ;;
+    3)
+      echo "離開..."
+      exit 0
+      ;;
+    *)
+      echo "無效的選擇。"
+      ;;
+  esac
 done
-
-configure_systemd_resolved
