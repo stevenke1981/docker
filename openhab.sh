367ef3928fd40881723b09d3bf2b58f9eb6cbdef
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-03-01 00:37:35 +0800

    Update openhab.sh

--- openhab.sh
+++ openhab.sh
@@ -2,19 +2,52 @@
 
 #ver 1.0.0
 
-# 創建 openHAB 用戶
+# 安裝或移除 openHAB
 
-sudo useradd -m openhab
+action="$1"
 
-# 創建 openHAB 配置、用戶數據和附加組件目錄
+# 安裝 openHAB
 
-sudo mkdir -p /etc/openhab /var/lib/openhab /usr/share/openhab/addons
+if [ "$action" = "install" ]; then
 
-# 將 openHAB 用戶添加到 docker 組
+  # 創建 openHAB 用戶
 
-sudo usermod -aG docker openhab
+  sudo useradd -m openhab
 
-# 啟動 openHAB
+  # 創建 openHAB 配置、用戶數據和附加組件目錄
 
-sudo docker run -it --rm --name openhab -p 9080:8080 -v /etc/openhab:/etc/openhab:ro -v /var/lib/openhab:/var/lib/openhab:rw -v /usr/share/openhab/addons:/usr/share/openhab/addons:rw openhab/openhab:latest
+  sudo mkdir -p /etc/openhab /var/lib/openhab /usr/share/openhab/addons
+
+  # 將 openHAB 用戶添加到 docker 組
+
+  sudo usermod -aG docker openhab
+
+  # 啟動 openHAB
+
+  sudo docker run -it --rm --name openhab -p 9080:8080 -v /etc/openhab:/etc/openhab:ro -v /var/lib/openhab:/var/lib/openhab:rw -v /usr/share/openhab/addons:/usr/share/openhab/addons:rw openhab/openhab:latest
+
+fi
+
+# 移除 openHAB
+
+if [ "$action" = "remove" ]; then
+
+  # 停止 openHAB 容器
+
+  sudo docker stop openhab
+
+  # 刪除 openHAB 容器
+
+  sudo docker rm openhab
+
+  # 刪除 openHAB 用戶
+
+  sudo userdel openhab
+
+  # 刪除 openHAB 配置、用戶數據和附加組件目錄
+
+  sudo rm -rf /etc/openhab /var/lib/openhab /usr/share/openhab/addons
+
+fi
+
 
