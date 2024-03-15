a85e51b783272d25cd8b49c272becad368d65be5
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-03-16 01:53:58 +0800

    Update check_docker.sh

--- check_docker.sh
+++ check_docker.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 check_docker() {
   if ! command -v docker &> /dev/null; then
@@ -8,4 +7,53 @@ check_docker() {
   else
     echo "Docker is already installed."
   fi
+}
+
+
+install_docker() {
+
+# 更新系統
+#sudo apt update
+
+# 安裝 Docker GPG 密鑰
+#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+# 添加 Docker 軟件倉庫
+#sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
+# 安裝 Docker
+#sudo apt install docker.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# 啟動 Docker 服務
+sudo systemctl start docker
+
+# 啟用 Docker 服務
+sudo systemctl enable docker
+
+# 創建 Docker 用戶組
+sudo groupadd docker
+
+# 將當前用戶添加到 Docker 用戶組
+sudo usermod -aG docker $USER
+
+# 輸出提示信息
+echo "Docker 已成功安裝。"
+echo "請重新啟動您的系統以使更改生效。"
+
 }
