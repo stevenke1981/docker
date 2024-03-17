eac1c3980b08e9e240cebeb64cf76f618882a090
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-03-17 12:47:29 +0800

    Update jellyfin.sh

--- jellyfin.sh
+++ jellyfin.sh
@@ -10,9 +10,20 @@ apt install exfat-utils -y
 # 定義 Jellyfin 配置和媒體存儲目錄的路徑
 jellyfin_config="$HOME/jellyfin/config"
 jellyfin_media="$HOME/jellyfin/media"
-jellyfin_tvshows="/dev/sd0/tvshows"
 
-function start_jellyfin {
+
+# 檢查外接硬碟是否存在
+if lsblk | grep -q /dev/sd0; then
+  echo "外接硬碟已連接。"
+  jellyfin_tvshows="/dev/sd0/tvshows"
+  start_jellyfin0
+else
+  echo "外接硬碟未連接。"
+  start_jellyfin1
+fi
+
+
+function start_jellyfin0 {
   # 創建配置和媒體存儲目錄
   mkdir -p $jellyfin_config
   mkdir -p $jellyfin_media
@@ -22,6 +33,26 @@ function start_jellyfin {
   --volume $jellyfin_config:/config --volume /tmp:/cache \
   --volume $jellyfin_media:/media \
   --volume $jellyfin_tvshows:/tvshows \
+  nyanmisaka/jellyfin:latest-rockchip
+
+  # 檢查 Jellyfin 容器是否成功運行
+  if docker ps | grep -q jellyfin; then
+    echo "Jellyfin 容器正在運行。"
+    echo "您現在可以通過瀏覽器訪問 http://$(hostname -I | awk '{print $1}'):8096 來使用 Jellyfin。"
+  else
+    echo "Jellyfin 容器未能成功啟動。請檢查日誌以獲取錯誤信息。"
+  fi
+}
+
+function start_jellyfin1 {
+  # 創建配置和媒體存儲目錄
+  mkdir -p $jellyfin_config
+  mkdir -p $jellyfin_media
+
+  # 運行 Jellyfin Docker 容器，使用先前定義的參數
+  docker run -d --name jellyfin --privileged -p 8096:8096 --restart=unless-stopped \
+  --volume $jellyfin_config:/config --volume /tmp:/cache \
+  --volume $jellyfin_media:/media \
   nyanmisaka/jellyfin:latest-rockchip
 
   # 檢查 Jellyfin 容器是否成功運行
