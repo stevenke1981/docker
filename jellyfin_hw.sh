4a5da5971247ae0e3d1e81e047abd7085928de6c
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-03-18 13:47:21 +0800

    Update jellyfin_hw.sh

--- jellyfin_hw.sh
+++ jellyfin_hw.sh
@@ -1,14 +1,14 @@
 docker run -d \
- --name jellyfin \
- --privileged \
- --net=host \
- --restart=unless-stopped \
- --volume $HOME/jellyfin/config:/config \
- --volume $HOME/jellyfin/cache:/cache \
- --volume $HOME/jellyfin/media:/media \
- `for dev in dri dma_heap mali0 rga mpp_service \
+--name jellyfinny \
+--privileged \
+-p 8096:8096 \
+--restart=unless-stopped \
+--volume $HOME/jellyfin/config:/config \
+--volume $HOME/jellyfin/cache:/cache \
+--volume $HOME/jellyfin/media:/media \
+`for dev in dri dma_heap mali0 rga mpp_service \
     iep mpp-service vpu_service vpu-service \
     hevc_service hevc-service rkvdec rkvenc vepu h265e ; do \
    [ -e "/dev/$dev" ] && echo " --device /dev/$dev"; \
   done` \
- nyanmisaka/jellyfin:latest-rockchip
+nyanmisaka/jellyfin:latest-rockchip
