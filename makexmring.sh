267d6c231901d25ebe803ad0bc524d0ffa0b72a7
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-03-21 00:46:53 +0800

    Update makexmring.sh

--- makexmring.sh
+++ makexmring.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
