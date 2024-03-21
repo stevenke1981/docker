f0d4e440fe9bb88c6ac1647a0d65b3ada5817827
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-03-21 13:04:25 +0800

    Update mymind.sh

--- mymind.sh
+++ mymind.sh
@@ -3,16 +3,17 @@
 # 獲取 CPU 型號
 
 cpu_model=$(cat /proc/cpuinfo | grep "model name" | awk '{print $4}')
+pool=randomxmonero.auto.nicehash.com:9200
 mywallet=38m2mrVGunLYreKxZq4t3hKufuaU97mDHK
 workname=x9903
 
 # 根據 CPU 型號選擇
 
 if [[ $cpu_model =~ "AMD" ]]; then
-  sudo ./xmrig -a rx/0 -o randomxmonero.auto.nicehash.com:9200 -u $mywallet.$workname -p x --nicehash --asm=ryzen --donate-level=1
+  sudo ./xmrig -a rx/0 -o $pool -u $mywallet.$workname -p x --nicehash --asm=ryzen --donate-level=1
   echo "使用AMD CPU開始計算"
 elif [[ $cpu_model =~ "Intel" ]]; then
-  sudo ./xmrig -a rx/0 -o randomxmonero.auto.nicehash.com:9200 -u $mywallet.$workname -p x --nicehash --donate-level=1
+  sudo ./xmrig -a rx/0 -o $pool -u $mywallet.$workname -p x --nicehash --donate-level=1
   echo "使用Intel CPU開始計算"
 else
   echo "無法識別 CPU 型號，請手動指定"
