baab9b5aaae390e70d613d2cefe37e3aa1d777ad
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-03-21 09:06:09 +0800

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -3,7 +3,7 @@
 # Update
 sudo apt update && sudo apt upgrade -y
 
-# Install and configure the display manager, another lightdm, gdm3,kdm
+# Install and configure the display manager, another lightdm, gdm3,kdm, sddm
 sudo apt install slim -y
 
 # Ubuntu desktop
