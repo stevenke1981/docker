afabad332d419cf508361d48fe1acd8550cc17fe
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-11-04 11:47:59 +0800

    Update smbmanage1.sh

--- smb/smbmanage1.sh
+++ smb/smbmanage1.sh
@@ -7,7 +7,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # 定義安裝位置
-local docker_dir="$HOME/dockerdata/web-smb"
+docker_dir="$HOME/dockerdata/web-smb"
 
 # 自動創建目錄的函式
 create_directory_if_not_exists() {
